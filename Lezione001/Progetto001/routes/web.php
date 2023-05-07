0d22dcd724f4b8b091de7a5bb92c05fe8f9c1e93
Emanuele Manera <emanuele.manera01@gmail.com>
2023-05-07 15:57:29 +0200

    Selfwork001: esercizi da 1 a 4

--- Lezione001/Progetto001/routes/web.php
+++ Lezione001/Progetto001/routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/stringa', function(){
+    return 'Sono un esempio di stringa sul tuo browser!';
+}); 
+
+Route::get('/intero', function(){
+    return 42; 
+}); 
+
+Route::get('/elemento array', function(){   // funziona inserendo carattere speciale spazio. URI: /elemento%20array
+    $fruttaDiStagione = ['pesca', 'ananas', 'banana']; 
+    return $fruttaDiStagione[2];
+}); 
+
+Route::get('/chi-siamo', function(){
+    return view('chisiamo');
+}); 
+
+Route::get('/contatti', function(){
+    return view('contatti');
+}); 
+
+Route::get('/storia', function(){
+    return view("storia");
+}); 
+
