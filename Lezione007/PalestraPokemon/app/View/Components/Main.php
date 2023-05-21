4f201750a24d862c857de34beb818c8fbdcc3b2b
Emanuele Manera <emanuele.manera01@gmail.com>
2023-05-21 20:32:59 +0200

    Palestra Pokemon: Main

---
+++ Lezione007/PalestraPokemon/app/View/Components/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Main extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        // Lezione007/PalestraPokemon/resources/views/
+        // layouts/main
+        // .blade.php 
+
+        return view('layouts.main');
+    }
+}
